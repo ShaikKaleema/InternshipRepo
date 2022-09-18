bd1596a8577ef2cab6895d6b6719cbd7e8a6eded
ShaikKaleema <shaikkaleema22@gmail.com>
2022-09-18 20:37:33 +0530

    Assignment2

--- I2R.py
+++ I2R.py
@@ -5,9 +5,11 @@ class Solution:
         rom=['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I']
         rom_num=""
         i=0
-        while num>0:
-            for _ in range(num//num1[i]):
-                rom_num=rom_num+rom[i]
-                num=num-num1[i]
-            i=i+1
+        while num:
+            div=num//num1[i]
+            num%=num1[i]
+            while div:
+                rom_num+=rom[i]
+                div-=1
+            i+=1
         return rom_num
