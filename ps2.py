f0c0aa73cfbf2ec61463c2e4a1ce9f522f81d125
ShaikKaleema <96244335+ShaikKaleema@users.noreply.github.com>
2022-10-07 01:49:04 +0530

    Assignment-3

--- ps2.py
+++ ps2.py
@@ -1,4 +1,9 @@
-#Add two numbers
+'''Problem : Add two numbers
+Problem Statement
+Every problem starts with a Problem Statement. It tells you in detail about the task to be solved.
+Shivam is the youngest programmer in the world, he is just 12 years old. Shivam is learning programming and today he is writing his first program.
+The task is very simple: given two integers A and B, write a program to add these two numbers and output it.'''
+# Code :
 T = int(input())
 for tc in range(T):
 	# Read integers a and b.
