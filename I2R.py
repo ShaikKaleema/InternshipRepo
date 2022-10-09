09b9e97629b64ae76cf94089405394fd5fead9ff
joelraj67 <31388901+joelraj67@users.noreply.github.com>
2022-10-09 17:46:29 +0530

    Update I2R.py

--- I2R.py
+++ I2R.py
@@ -13,3 +13,38 @@ class Solution:
                 div-=1
             i+=1
         return rom_num
+    
+
+
+#Created a list of dictionary Elements Instead Of Two array.
+#def intToRoman(num: int) -> str:
+#        defaultList= [
+#          {"romanSymbl":"M","NumericalValue":1000},
+#          {"romanSymbl":"CM","NumericalValue":900},
+#          {"romanSymbl":"D","NumericalValue":500},
+#          {"romanSymbl":"CD","NumericalValue":400},
+#          {"romanSymbl":"C","NumericalValue":100},
+#          {"romanSymbl":"XC","NumericalValue":90},
+#          {"romanSymbl":"L","NumericalValue":50},
+#          {"romanSymbl":"XL","NumericalValue":40},
+#          {"romanSymbl":"X","NumericalValue":10},
+#          {"romanSymbl":"IX","NumericalValue":9},
+#          {"romanSymbl":"V","NumericalValue":5},
+#          {"romanSymbl":"IV","NumericalValue":4},
+#          {"romanSymbl":"I","NumericalValue":1},
+#        ] 
+#        rom_num=""
+#        i=0
+#        while num:
+#            div=num//int(defaultList[i]["NumericalValue"])
+#            num%=int(defaultList[i]["NumericalValue"])
+#            while div:
+#                rom_num+=defaultList[i]["romanSymbl"]
+#                div-=1
+#            i+=1
+#        return rom_num
+#print(intToRoman(50))
+
+
+
+    
