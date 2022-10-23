a85e7e81e87882619a11cece62945867c9f5c3ea
ShaikKaleema <shaikkaleema22@gmail.com>
2022-10-24 00:28:50 +0530

    Assignment-3

--- ps6.py
+++ ps6.py
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 #Odd repeat
 T=int(input())
 for i in range(T):
     N,K,S=map(int,input().split())
     S=S-(N*N)
-=======
-#Odd repeat
-T=int(input())
-for i in range(T):
-    N,K,S=map(int,input().split())
-    S=S-(N*N)
->>>>>>> 97be87de4448efbba274d675595b0f1206059fbc
     print(int(S/(K-1)))
